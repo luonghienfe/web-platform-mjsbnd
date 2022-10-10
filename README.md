e09156ce35430bb95e179b6f08708d73cd4f46f0
luonghienfe <115452777+luonghienfe@users.noreply.github.com>
2022-10-10 14:32:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-mjsbnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-mjsbnd)
---
+++ main.js
@@ -0,0 +1,38 @@
+/** 1
+ * // hàm alert: bật thông báo popbub
+ * // alert('Hi javascript basic!');
+ */
+
+/** 2
+ * // khai bao bien
+ * var fullName = 'Nguyen Van Hien';
+ * var age = 20; // khai bao tuoi
+ *
+ * // alert chỉ hiện thị 1 cái tại 1 thời điểm
+ * alert(fullName);
+ * alert(age);
+ *
+ */
+
+/** 3
+ * // Các bạn xem ví dụ tại đây:
+ * // 1. Dưới đây là một lời gọi hàm alert() bị comments,
+ * // vì vậy, không có hộp thoại nào xuất hiện.
+ * // 👉 Bạn có thể uncomment để thấy hộp thoại được bật lên.
+ alert('Xin chào anh em nhé!');
+
+/* 
+Đây là cú pháp comment trên nhiều dòng
+ */
+
+/** Cách trình bày đẹp cho cú pháp comments
+ * trên nhiều dòng là như thế này
+ *
+ */
+
+/** 4
+ * built-in chú ý đối tượng truyền vào
+ */
+
+
+
